8bd6e91e989c14fc983fc22e18d85ce1d97ba738
Mamelska <marta.mamelska@gmail.com>
2017-05-27 11:37:11 +0200

    Added new scenes: Game1, Menu and script Change scene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    //Ładowanie wybranej sceny
+    public void ChangeSceneTo(int ChangeScene)
+    {
+
+        SceneManager.LoadScene(ChangeScene);
+
+    }
+
+
+
+
+}
