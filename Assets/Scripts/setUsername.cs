ee8f2237c2d118be706b30f0360659a580f82c1e
Piotr Pawelczyk <piopawelczyk@gmail.com>
2017-05-27 06:28:42 +0200

    Added first scripts to scene0

---
+++ Assets/Scripts/setUsername.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class setUsername : MonoBehaviour {
+
+    public Dropdown dropdown;
+    public InputField inputField;
+    public void dropdownValueChanged(int index)
+    {
+        List<Dropdown.OptionData> dropdownOptions = dropdown.GetComponent<Dropdown>().options;
+        inputField.text = dropdownOptions[index].text;
+    }
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+            
+    }
+}
